f48c49c6cee8c45715fe17dc6a352ca9c93c77be
pxb123go <sahoopradyun@gmail.com>
2024-09-18 20:25:11 +0300

    initial commit

---
+++ Proggram.java
@@ -0,0 +1,12 @@
+class Proggram{
+    public static void main(String[] args) {
+        int a=7;
+        int b=10;
+        int c=a>b?a:b;
+        System.out.println(a++);
+        System.out.println(++b);
+        System.out.println("Ternary Operator");
+        System.out.println("Largest Number is" + c);
+
+    }
+}
